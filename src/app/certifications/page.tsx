324ab5ee760254437dd53375d00720ec7b648345
HARSH KUMAR <xt.harshh@gmail.com>
2025-02-03 00:11:42 +0530

    new

--- src/app/certifications/page.tsx
+++ src/app/certifications/page.tsx
@@ -2,10 +2,9 @@
 
 import { motion } from "framer-motion"
 import {
-    SiInfosys,       // Infosys
-       // NPTEL (Assuming an icon is available)
-    SiAmazon,     // AWS
-    SiUdemy,         // Udemy
+    SiInfosys,       
+    SiAmazon,     
+    SiUdemy,        
   } from 'react-icons/si';
   
   
